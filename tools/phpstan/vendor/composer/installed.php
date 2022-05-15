ac793b3767890409ce7f951928f11bbe99e27d96
vectortracey <miek1128@hotmail.com>
2022-05-15 16:16:16 +0100

    added tools

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4bd71677175fdc6819a4b6a059bf8623d5009fad',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4bd71677175fdc6819a4b6a059bf8623d5009fad',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.6.8',
+            'version' => '1.6.8.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => 'd76498c5531232cb8386ceb6004f7e013138d3ba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
